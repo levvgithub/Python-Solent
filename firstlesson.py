b9980c3a06493af300efdbbd4f94ac1c9f2ea561
levvgithub <leva.rubinov@gmail.com>
2023-11-12 19:57:39 +0300

    Create firstlesson.py

---
+++ firstlesson.py
@@ -0,0 +1,24 @@
+name = "Leva"
+print(f"Hello {name}")
+
+name = "Leva"
+occupation = "Student"
+
+print(f"My name is {name},", end = "")
+print(f"I'm a {occupation}")
+
+message = "python"
+
+print(f"right-justify: {message: >8}")
+print(f"center: {message: ^10}")
+print(f"left-justify: {message: <10}")
+
+average = 4.4444
+print(f"Three decimal places: {average:.2f}")
+
+print("Leva")
+name = input()
+print(f"It is nice to meet you, {name}")
+
+weight = float(input("Enter weight: "))
+weight = int(input("Enter age: "))
